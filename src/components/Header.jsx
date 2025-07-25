14fa648f50930bfa0dd4cd814f5da9701ffdb251
Annelies Malzer <anneliesarbouw@gmail.com>
2025-07-25 15:53:23 +0200

    update all

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,17 @@ import { Box, Heading } from '@chakra-ui/react';
 const Header = ({ greeting }) => {
   return (
     <Box
-      bg="#f0f4e2"
+      bg="#fff"
+      borderBottom="1px solid #EFEEEA"
       py={16}
       px={4}
-      width="100%"
+      width="1200px"
+      mx="auto"
       position="relative"
       left="0"
       textAlign="center"
     >
-      <Heading as="h1" size="2xl" color="#447632">
+      <Heading as="h1" size="2xl" color="#447632" bg="rgba(255, 255, 255, 0.8)" display="inline-block" px={4} py={2} borderRadius="md">
         {greeting}
       </Heading>
     </Box>
